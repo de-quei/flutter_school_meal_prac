6617379204df2c9d1c3f082f9471621d0d47b9cc
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-16 14:31:50 +0900

    입력란을 생성합니다.

--- lib/screens/insert_meal_screen.dart
+++ lib/screens/insert_meal_screen.dart
@@ -8,6 +8,10 @@ class InsertMealScreen extends StatefulWidget {
 }
 
 class _InsertMealScreenState extends State<InsertMealScreen> {
+  final TextEditingController breakfastController = TextEditingController();
+  final TextEditingController lunchController = TextEditingController();
+  final TextEditingController dinnerController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,19 +27,59 @@ class _InsertMealScreenState extends State<InsertMealScreen> {
           ),
           centerTitle: true,
         ),
-        body: const SingleChildScrollView(
-          padding: EdgeInsets.all(20.0),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Text(
+              const Text(
                 "오늘 급식을 입력해주세요!",
                 style: TextStyle(fontSize: 20),
+              ),
+              const SizedBox(height: 20),
+              buildMealInputForm("조식", breakfastController),
+              const SizedBox(height: 20),
+              buildMealInputForm("중식", lunchController),
+              const SizedBox(height: 20),
+              buildMealInputForm("석식", dinnerController),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                // 적용 버튼
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF188852),
+                  fixedSize: Size(MediaQuery.of(context).size.width, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: const Text(
+                  "적용하기",
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildMealInputForm(String mealType, TextEditingController controller) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "$mealType 입력",
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        TextFormField(
+          controller: controller,
+          decoration: InputDecoration(
+            hintText: "$mealType을 입력하세요",
+          ),
+        ),
+      ],
     );
   }
 }
