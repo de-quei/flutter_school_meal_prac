958bcd4ae94bb7948931a2271f2732fadbaef36a
Hyunji Kim <s2208@e-mirim.hs.kr>
2024-04-16 10:57:25 +0900

    버튼을 화면 하단에 고정합니다.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -20,18 +20,27 @@ class HomeScreen extends StatelessWidget {
         centerTitle: true, //title의 위치를 center로 설정
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text(
-              "급식 등록하기",
-              style: TextStyle(color: Colors.white),
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Color(0xFF188852),
-              fixedSize: Size(MediaQuery.of(context).size.width, 50),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          // Expanded 위젯 추가
+          Expanded(
+            child: Container(),
+          ),
+          Container(
+            margin: const EdgeInsets.all(10.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF188852),
+                fixedSize: Size(MediaQuery.of(context).size.width, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: const Text(
+                "급식 등록하기",
+                style: TextStyle(color: Colors.white),
               ),
             ),
           ),
